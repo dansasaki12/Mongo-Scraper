491c14fd8fdb499f9de100951f4f269f40ede7e4
Dan <dan.sasaki12@gmail.com>
2018-06-19 18:01:02 -0600

    updating

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var logger = require("morgan");
 var cheerio = require("cheerio");
 var path = require("path");
 var app = express();
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -19,18 +19,14 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("./public"));
 
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.mongolab-objective-37338 || "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
+// connect to database
 mongoose.Promise = Promise;
-mongoose.connect(mongolab-objective-37338);
-// if(process.env.MONGODB_URI) {
-//     mongoose.connect(process.env.MONGODB_URI)
-// } else {
-//     mongoose.connect(dbConnect);
-// }
+var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect(dbConnect);
+}
 
 var db = mongoose.connection;
 db.on('error',function(err){
@@ -63,4 +59,10 @@ app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
