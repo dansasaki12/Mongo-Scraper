90fd8911e6cac66ec8f22827f92cc2ecb15caa74
Dan <dan.sasaki12@gmail.com>
2018-06-23 11:47:22 -0600

    update

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ if(process.env.MONGODB_URI) {
     mongoose.connect(dbConnect);
 }
 
+
 var db = mongoose.connection;
 db.on('error',function(err){
     console.log('Mongoose Error',err);
@@ -60,10 +61,3 @@ app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
